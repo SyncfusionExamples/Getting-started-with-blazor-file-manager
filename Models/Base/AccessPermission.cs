c348843cd654688461525863f539121a0f60d8c9
Muthukrishnan Kandasamy <muthu555krish@gmail.com>
2021-08-05 18:09:01 +0530

    Syncfusion Blazor getting started sample

---
+++ Models/Base/AccessPermission.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+#if EJ2_DNX
+using System.Web;
+#endif
+
+namespace Syncfusion.EJ2.FileManager.Base
+{
+    public class AccessPermission
+    {
+        public bool Copy = true;
+        public bool Download = true;
+        public bool Write = true;
+        public bool WriteContents = true;
+        public bool Read = true;
+        public bool Upload = true;
+        public string Message = String.Empty;
+    }
+}
